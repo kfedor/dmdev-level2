500c71e346f4d2264a008cba6427622138379ffd
Fedor <shlakmail@list.ru>
2023-02-13 11:32:56 +0600

    homework-week1 (1.0.0)

---
+++ service/src/main/java/com/nutscoding/service/App.java
@@ -0,0 +1,11 @@
+package com.nutscoding.service;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public void printSmth() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ service/src/test/java/com/nutscoding/integration/ExampleIT.java
@@ -0,0 +1,13 @@
+package com.nutscoding.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
